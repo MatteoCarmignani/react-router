7272041db60f0c566ddad0c534e134961e9d55b6
MatteoCarmignani <matteo.carmignani01@gmail.com>
2025-10-06 19:19:31 +0200

    completato

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,8 +1,14 @@
 const HomePage = () => {
     return (
-        <>
-            <div>Ciao, sono la Homepage</div>
-        </>
+        <section className="welcome-section">
+            <div className="welcome-box">
+                <h1>Benvenuto nel nostro Store</h1>
+                <p>
+                Esplora i nostri prodotti, scopri le offerte e lasciati ispirare dalle ultime novità.
+                </p>
+                <a href="/product" className="welcome-btn">Scopri i prodotti</a>
+            </div>
+        </section>
     )
 }
 
